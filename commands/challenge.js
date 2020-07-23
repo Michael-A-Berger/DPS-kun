e100372ef9edf0911056f9d7a4836aa6b891f767
Michael Berger <michael.anthony.berger@gmail.com>
2020-07-23 08:36:37 -0400

    Began reworking [challenge.js] to function independent of challenge files

--- commands/challenge.js
+++ commands/challenge.js
@@ -1,3 +1,135 @@
+// Modules
+const fs = require('fs');
+const database = require(`${__dirname}/../database.js`);
+let grooveCoasterPC = require(`${__dirname}/challenge-files/groove-coaster-pc.js`);
+grooveCoasterPC = grooveCoasterPC.challenge;
+let piuPrime2 = require(`${__dirname}/challenge-files/piu-prime-2.js`);
+piuPrime2 = piuPrime2.challenge;
+
+// Constant Variables
+const filesLocation = `${__dirname}/challenge-files`;
+const gcpcIdentities = ['groovecoasterpc', 'gcpc'];
+const museDashIdentities = ['musedash', 'msds'];
+const piuPrime2Identities = ['piuprime2', 'prime2'];
+
+// supportedGames()
+function supportedGames(message){
+  const gcpcString = gcpcIdentities.toString().replace(',', ', ');
+  const msdsString = museDashIdentities.toString().replace(',', ', ');
+  const prime2String = piuPrime2Identities.toString().replace(',', ', ');
+  const support = 'Supported Games:\n```'
+                  + 'Groove Coaster PC:    [' + gcpcstring + ']\n'
+                  + 'Muse Dash:            [' + msdsString + ']\n'
+                  + 'Pump It Up Prime 2:   [' + prime2String + ']\n'
+                  + '```';
+  return support;
+}
+
+// grooveCoasterPCChallenge()
+function grooveCoasterPcChallenge(message){
+  grooveCoasterPC.load();
+  return grooveCoasterPC.get(message);
+}
+
+// museDashChallenge()
+function museDashChallenge(message){
+  // Defining the return string + the song + the valid songs
+  let returnString;
+  let chosenSong;
+  let validSongs = [];
+
+  // Getting the help message if requested, otherwise searching the songs
+  if (message.content.endsWith('help')) {
+    returnString = database.MuseDashHelp();
+  } else {
+    validSongs = database.SearchMuseDash(message.content);
+  }
+
+  // IF the valid songs array is longer than one song...
+  if (validSongs.length > 1) {
+    chosenSong = validSongs[Math.round(Math.random() * (validSongs.length - 1))];
+  } else if (validSongs.length === 1) { chosenSong = validSongs[0]; }
+
+  // IF a song was chosen, format the return string
+  if (chosenSong !== undefined) {
+    returnString = `\:womans_hat: <@${message.author.id}> 's CHALLENGE \:womans_hat:\nPlay`;
+    if (message.content.toLowerCase().indexOf('hidden') > -1) {
+      returnString += ` the **Hidden (${chosenSong.hidden})** chart of`;
+    } else if (message.content.toLowerCase().indexOf('master') > -1) {
+      returnString += ` the **Master (${chosenSong.master})** chart of`;
+    } else if (message.content.toLowerCase().indexOf('hard') > -1) {
+      returnString += ` the **Hard (${chosenSong.hard})** chart of`;
+    } else if (message.content.toLowerCase().indexOf('easy') > -1) {
+      returnString += ` the **Easy (${chosenSong.easy})** chart of`;
+    }
+    returnString += ` **${chosenSong.name}** (by ${chosenSong.artist})`;
+    if (message.content.toLowerCase().indexOf('bpm') > -1) { returnString += ` (BPM: ${chosenSong.bpm})`; }
+    returnString += `\n(Pack: ${chosenSong.pack})`;
+  }
+  
+  // IF the return string has not been defined yet... (no matches found)
+  if (returnString === undefined){
+    returnString = '\:open_file_folder: No songs could be found with those restrictions! '
+                  + 'Enter `challenge musedash help` to see the list of valid challenge '
+                  + 'options for this game.';
+  }
+
+  // Returning the return string
+  return returnString;
+}
+
+// piuPrime2Challenge()
+function piuPrime2Challenge(message){
+  piuPrime2.load();
+  return piuPrime2.get(message);
+}
+
+// challenge()
+function challenge(message) {
+  let found = false;
+  let stringToPrint = '';
+  
+  // Groove Coaster PC
+  for (let num = 0; !found && num < gcpcIdentities.length; num++){
+    if (message.content.startsWith(gcpcIdentities[num])){
+      stringToPrint = grooveCoasterPcChallenge(message)
+      found = true;
+    }
+  }
+  
+  // Muse Dash
+  for (let num = 0; !found && num < museDashIdentities.length; num++){
+    if (message.content.startsWith(museDashIdentities[num])){
+      stringToPrint = museDashChallenge(message);
+      found = true;
+    }
+  }
+  
+  // PIU Prime 2
+  for (let num = 0; !found && num < piuPrime2Identities.length; num++){
+    if (message.content.startsWith(piuPrime2Identities[num])){
+      stringToPrint = piuPrime2Challenge(message);
+      found = true;
+    }
+  }
+  
+  // List of Supported Games
+  if (message.content === 'support') {
+    stringToPrint = supportedGames();
+    found = true;
+  }
+
+  if (!found) {
+    stringToPrint = 'Proper Usage:\n```challenge [game] [criteria?]\n\n'
+						+ "[game]       = The game to choose the chart from ('support' lists all supported "
+						+ 'games)\n'
+						+ "[criteria?]  = Restricts chosen song to certain criteria (issue 'challenge [game] "
+						+ "help' to see options)```";
+  }
+
+  message.channel.send(stringToPrint);
+}
+
 // Exported Command
 module.exports.command = {
   keywords: ['chll', 'challenge'],
@@ -7,68 +139,3 @@ module.exports.command = {
   secret: false,
   action: challenge,
 };
-
-// Modules
-const fs = require('fs');
-
-// Constant Variables
-const filesLocation = `${__dirname}/challenge-files`;
-
-// Command Variables
-const games = [];
-
-// loadChallengeFiles()
-function LoadChallengeFiles() {
-  console.log('-- Loading the challenge files...');
-  let counter = 0;
-  fs.readdirSync(filesLocation).forEach((file) => {
-    if (file.endsWith('.js')) {
-      const challengeFile = require(`${filesLocation}/${file}`);
-      challengeFile.challenge.load();
-      games[counter] = challengeFile.challenge;
-      counter++;
-    }
-  });
-}
-LoadChallengeFiles();
-
-// challenge()
-function challenge(message) {
-  let found = false;
-  let stringToPrint = '';
-
-  for (var num = 0; num < games.length && !found; num++) {
-    for (var n = 0; n < games[num].identities.length && !found; n++) {
-      if (message.content.startsWith(games[num].identities[n])) {
-        message.content = message.content.replace(games[num].identities[n], '').trim();
-        console.log(`-- [${message.content}]`);
-
-        stringToPrint = games[num].get(message);
-
-        found = true;
-      }
-    }
-  }
-
-  if (message.content === 'support') {
-    stringToPrint += 'Supported Games:\n```';
-    for (var num = 0; num < games.length; num++) {
-      stringToPrint += `${games[num].title}     [`;
-      for (var n = 0; n < games[num].identities.length; n++) { stringToPrint += games[num].identities[n] + (n + 1 < games[num].identities.length ? ', ' : ']\n'); }
-    }
-    stringToPrint += '```';
-
-    found = true;
-  }
-
-  if (!found) {
-    stringToPrint = 'Proper Usage:\n```challenge [game] [level] [gimmick?]\n\n'
-						+ "[game]       = The game to choose the chart from ('support' lists all supported "
-						+ 'games)\n'
-						+ "[level]      = The difficulty of the chart ('~' increases dificulty range by 1)\n"
-						+ "[gimmick?]   = Enables/specifies gameplay modifiers (issue 'challenge [game] "
-						+ "help' to see options)```";
-  }
-
-  message.channel.send(stringToPrint);
-}
--- database.js
+++ database.js
@@ -96,6 +96,271 @@ function loadPiuPrime2() {
   console.log('-- Pump It Up Prime 2 songs loaded!');
 }
 
+// searchMuseDash()
+function searchMuseDash(paramString){
+  // Defining the returning array
+  let songMatches = [];
+  
+  // Splitting up the parameter string
+  const params = paramString.toLowerCase().split(' ');
+  
+  // =================================
+  // ===== PARSING SONG CRITERIA =====
+  // =================================
+
+  // Defining the match variables
+  let nameToMatch = '';
+  let exactName;
+  let artistToMatch = '';
+  let exactArtist;
+  let bpmToMatch = '';
+  let exactBpm;
+  let easyToMatch = NaN;
+  let exactEasy;
+  let hardToMatch = NaN;
+  let exactHard;
+  let masterToMatch = NaN;
+  let exactMaster;
+  let hiddenToMatch = NaN;
+  let exactHidden;
+  let packToMatch = '';
+  let exactPack;
+
+  // Processing the passed parameters
+  let currentParam = '';
+  let colonPos = -1;
+  let paramFound = false;
+  for (let num = 0; num < params.length; num++) {
+    // Getting the current parameter
+    currentParam = params[num];
+    paramFound = false;
+
+    // Name
+    if (!paramFound && currentParam.startsWith('name')) {
+      paramFound = true;
+      colonPos = currentParam.indexOf(':');
+      if (colonPos !== -1) {
+        exactName = true;
+        nameToMatch = currentParam.substr(colonPos + 1);
+      } else { exactName = false; }
+    }
+
+    // Artist
+    if (!paramFound && currentParam.startsWith('artist')) {
+      paramFound = true;
+      colonPos = currentParam.indexOf(':');
+      if (colonPos !== -1) {
+        exactArtist = true;
+        artistToMatch = currentParam.substr(colonPos + 1);
+      } else { exactArtist = false; }
+    }
+
+    // BPM
+    if (!paramFound && currentParam.startsWith('bpm')) {
+      paramFound = true;
+      colonPos = currentParam.indexOf(':');
+      if (colonPos !== -1) {
+        if (currentParam[colonPos + 1] !== '~') { exactBpm = true; } else { exactBpm = false; }
+        bpmToMatch = currentParam.substr(colonPos + (exactBpm ? 1 : 2));
+      } else { exactBpm = false; }
+    }
+
+    // Easy
+    if (!paramFound && currentParam.startsWith('easy')) {
+      paramFound = true;
+      colonPos = currentParam.indexOf(':');
+      if (colonPos !== -1) {
+        if (currentParam[colonPos + 1] !== '~') { exactEasy = true; } else { exactEasy = false; }
+        easyToMatch = parseInt(currentParam.substr(colonPos + (exactEasy ? 1 : 2)), 10);
+      } else { exactEasy = false; }
+    }
+
+    // Hard
+    if (!paramFound && currentParam.startsWith('hard')) {
+      paramFound = true;
+      colonPos = currentParam.indexOf(':');
+      if (colonPos !== -1) {
+        if (currentParam[colonPos + 1] !== '~') { exactHard = true; } else { exactHard = false; }
+        hardToMatch = parseInt(currentParam.substr(colonPos + (exactHard ? 1 : 2)), 10);
+      } else { exactHard = false; }
+    }
+
+    // Master
+    if (!paramFound && currentParam.startsWith('master')) {
+      paramFound = true;
+      colonPos = currentParam.indexOf(':');
+      if (colonPos !== -1) {
+        if (currentParam[colonPos + 1] !== '~') { exactMaster = true; } else { exactMaster = false; }
+        masterToMatch = parseInt(currentParam.substr(colonPos + (exactMaster ? 1 : 2)), 10);
+      } else { exactMaster = false; }
+    }
+
+    // Hidden
+    if (!paramFound && currentParam.startsWith('hidden')) {
+      paramFound = true;
+      colonPos = currentParam.indexOf(':');
+      if (colonPos !== -1) {
+        if (currentParam[colonPos + 1] !== '~') { exactHidden = true; } else { exactHidden = false; }
+        hiddenToMatch = parseInt(currentParam.substr(colonPos + (exactHidden ? 1 : 2)), 10);
+      } else { exactHidden = false; }
+    }
+
+    // Pack
+    if (!paramFound && currentParam.startsWith('pack')) {
+      paramFound = true;
+      colonPos = currentParam.indexOf(':');
+      if (colonPos !== -1) {
+        exactPack = true;
+        packToMatch = currentParam.substr(colonPos + 1);
+      } else { exactPack = false; }
+    }
+  }
+
+  // ==================================
+  // ===== GETTING MATCHING SONGS =====
+  // ==================================
+  let criteriaMet = true;
+  songMatches = museDashSongs.filter((song) => {
+    // Resetting the Criteria Met boolean
+    criteriaMet = true;
+
+    // Name
+    if (exactName !== undefined) {
+      if (exactName === true) {
+        criteriaMet = criteriaMet && (song.name.toLowerCase().indexOf(nameToMatch) > -1);
+      } else {
+        criteriaMet = criteriaMet && (song.name.length > 0);
+      }
+    }
+
+    // Artist
+    if (exactArtist !== undefined) {
+      if (exactArtist === true) {
+        criteriaMet = criteriaMet && (song.artist.toLowerCase().indexOf(artistToMatch) > -1);
+      } else {
+        criteriaMet = criteriaMet && (song.artist.length > 0);
+      }
+    }
+
+    // BPM
+    if (exactBpm !== undefined) {
+      const matchBpm = parseInt(bpmToMatch.replace(/[^\d]/g, ''), 10);
+      const songBpm = parseInt(song.bpm.replace(/[^\d]/g, ''), 10);
+      if (exactBpm === true) {
+        criteriaMet = criteriaMet && (matchBpm === songBpm);
+      } else {
+        criteriaMet = criteriaMet && (matchBpm - 10 <= songBpm && matchBpm + 10 >= songBpm);
+      }
+    }
+
+    // Easy
+    if (exactEasy !== undefined) {
+      if (!Number.isNaN(easyToMatch)) {
+        if (exactEasy === true) {
+          // Exact Match
+          const test = (song.easy === easyToMatch);
+          criteriaMet = criteriaMet && test;
+        } else {
+          // Range Match
+          const test = (easyToMatch - 1 <= song.easy && easyToMatch + 1 >= song.easy);
+          criteriaMet = criteriaMet && test;
+        }
+      } else {
+        // Property Exists
+        const test = !Number.isNaN(song.easy);
+        criteriaMet = criteriaMet && test;
+      }
+    }
+
+    // Hard
+    if (exactHard !== undefined) {
+      if (!Number.isNaN(hardToMatch)) {
+        if (exactHard === true) {
+          // Exact Match
+          const test = (song.hard === hardToMatch);
+          criteriaMet = criteriaMet && test;
+        } else {
+          // Range Match
+          const test = (hardToMatch - 1 <= song.hard && hardToMatch + 1 >= song.hard);
+          criteriaMet = criteriaMet && test;
+        }
+      } else {
+        // Property Exists
+        const test = !Number.isNaN(song.hard);
+        criteriaMet = criteriaMet && test;
+      }
+    }
+
+    // Master
+    if (exactMaster !== undefined) {
+      if (!Number.isNaN(masterToMatch)) {
+        if (exactMaster === true) {
+          // Exact Match
+          const test = (song.master === masterToMatch);
+          criteriaMet = criteriaMet && test;
+        } else {
+          // Range Match
+          const test = (masterToMatch - 1 <= song.master && masterToMatch + 1 >= song.master);
+          criteriaMet = criteriaMet && test;
+        }
+      } else {
+        // Property Exists
+        const test = !Number.isNaN(song.master);
+        criteriaMet = criteriaMet && test;
+      }
+    }
+
+    // Hidden
+    if (exactHidden !== undefined) {
+      if (!Number.isNaN(hiddenToMatch)) {
+        if (exactHidden === true) {
+          // Exact Match
+          const test = (song.hidden === hiddenToMatch);
+          criteriaMet = criteriaMet && test;
+        } else {
+          // Range Match
+          const test = (hiddenToMatch - 1 <= song.hidden && hiddenToMatch + 1 >= song.hidden);
+          criteriaMet = criteriaMet && test;
+        }
+      } else {
+        // Property Exists
+        const test = !Number.isNaN(song.hidden);
+        criteriaMet = criteriaMet && test;
+      }
+    }
+
+    // Pack
+    if (exactPack !== undefined) {
+      if (exactPack === true) {
+        criteriaMet = criteriaMet && (song.pack.toLowerCase().indexOf(packToMatch) > -1);
+      } else {
+        criteriaMet = criteriaMet && (song.pack.length > 0);
+      }
+    }
+
+    return criteriaMet;
+  });
+  
+  // Returning the array of matching songs
+  return songMatches;
+}
+
+// museDashHelp()
+function museDashHelp(){
+  const help = 'Proper Usage:\n```challenge musedash [name:?] [artist:?] [bpm:?] [easy] [hard] '
+              + '[master] [hidden] [pack:?]\n\n'
+              + '[name:?]    = Song name contains \'?\' (no spaces)\n'
+              + '[artist:?]  = Song artist name contains \'?\' (no spaces)\n'
+              + '[bpm:?]     = Song\'s BPM exactly matches \'?\' (prepend \'~\' for range of -/+ 10 BPM)\n'
+              + '[easy]      = Song must have an Easy difficulty chart (append \':?\' for exact difficulty, \':~?\' for range)\n'
+              + '[hard]      = Song must have a Hard difficulty chart (append \':?\' for exact difficulty, \':~?\' for range)\n'
+              + '[master]    = Song must have a Master difficulty chart (append \':?\' for exact difficulty, \':~?\' for range)\n'
+              + '[hidden]    = Song must have a Hidden difficulty chart (append \':?\' for exact difficulty, \':~?\' for range)\n'
+              + '[pack:?]    = Song must come from a pack whose name contains \'?\' (no spaces)\n'
+              + '```';
+  return help;
+}
+
 /* =============================
  * ===== GENERIC FUNCTIONS =====
  * =============================
@@ -111,7 +376,12 @@ function loadSongs() {
 // Setting up the exports
 module.exports = {
   LoadSongs: loadSongs,
+  // Song Arrays
   MuseDash: museDashSongs,
   PIUPrime2: piuPrime2Songs,
   GrooveCoasterPC: grooveCoasterPcSongs,
+  // Search Functions
+  SearchMuseDash: searchMuseDash,
+  // Help Functions
+  MuseDashHelp: museDashHelp,
 };
